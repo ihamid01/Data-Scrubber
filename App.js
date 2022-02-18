f1ba9c98f8b64744af4b438c7eef8e2eef19b1e0
ihamid01 <64386261+ihamid01@users.noreply.github.com>
2022-02-18 03:52:15 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,42 @@
 
 import { Table } from 'antd';
 import 'antd/dist/antd.min.css';
-import { columns, data } from './dataSource';
+import { columns } from './dataSource';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return <Table dataSource={data} columns={columns} pagination={false} />;
+  const [dataSource, setDataSource] = useState([]);
+  // const [totalPages, setTotalPages] = useState(1);
+
+  useEffect(() => {
+    fetchRecords(1);
+  }, []);
+
+  const fetchRecords = () => {
+    // setLoading(true);
+    axios
+      .get(`http://127.0.0.1:5000/data/`)
+      .then((res) => {
+        setDataSource(res.data);
+        // setTotalPages(res.data.totalPages);
+      });
+  };
+
+    // const fetchRecords = () => {
+    //   fetch('http://127.0.0.1:5000/data/').then(res=> {
+    //       res.json().then(response => {
+    //         console.log(response)
+    //       });
+    //     }
+    //   );
+    // };
+
+  return (
+
+    <Table dataSource={dataSource} columns={columns} pagination={false} />
+  )
+  
 }
 
 export default App;

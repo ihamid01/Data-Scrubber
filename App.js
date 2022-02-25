a6a23f18c4809c87c995872b3c7e3e689c58d3f3
ihamid01 <64386261+ihamid01@users.noreply.github.com>
2022-02-25 15:49:25 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,40 +1,49 @@
 
 import { Table } from 'antd';
 import 'antd/dist/antd.min.css';
+import './App.css';
 import { columns } from './dataSource';
 import { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
   const [dataSource, setDataSource] = useState([]);
-  // const [totalPages, setTotalPages] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [loading, setLoading] = useState(false);
+
 
   useEffect(() => {
     fetchRecords(1);
   }, []);
 
   const fetchRecords = () => {
-    // setLoading(true);
     axios
       .get(`http://127.0.0.1:5000/data/`)
       .then((res) => {
         setDataSource(res.data);
+        setTotalPages(res.data.totalPages);
+        setLoading(false);
         // setTotalPages(res.data.totalPages);
       });
   };
 
-    // const fetchRecords = () => {
-    //   fetch('http://127.0.0.1:5000/data/').then(res=> {
-    //       res.json().then(response => {
-    //         console.log(response)
-    //       });
-    //     }
-    //   );
-    // };
-
   return (
 
-    <Table dataSource={dataSource} columns={columns} pagination={false} />
+    // <><div className='App'>
+    //   <Data/>
+    // </div>
+    <Table
+    loading={loading}
+    columns={columns}
+    dataSource={dataSource}
+    pagination={{
+      pageSize: 10,
+      total: totalPages,
+      onChange: (page) => {
+        fetchRecords(page);
+      },
+    }}
+  ></Table>
   )
   
 }
